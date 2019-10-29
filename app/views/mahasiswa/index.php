fa0e67cfc2b32f6aee57315245b9c1ed17bea281
ajeng gustina <gustinaajeng26@gmail.com>
2019-10-29 09:39:52 +0700

    tugas phpmvc

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,89 +1,98 @@
 <div class="container mt-3">
 
+  <div class="row">
+    <div class= "col-lg-6">
+      <?php Flasher::flash(); ?>
+    </div>
+  </div>
 
-<div class="row">
+  <div class="row mb-3">
+    <div class= "col-lg-6">
+      <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#forModal">Tambah Data Mahasiswa
+      </button>
+    </div>
+  </div>
 
-   <div class="col-lg-6">
-		<?php Flasher::flash(); ?>
- 
-   </div>
+  <div class="row">
+    <div class= "col-lg-6">
+      <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="cari mahasiswa.." name="keyword" id="keyword" autocomplete="off" >
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary" type="submit" id="tombolCari">Cari</button>
+            </div>
+        </div>
+      </form>
+    </div>
+  </div>
 
-</div>
+  <div class ="row">
+    <div class="col-lg-6">
+    
+      <h2>Daftar Mahasiswa</h2>
+    
+        <ul class="list-group">
+      <?php foreach ($data['mhs'] as $mhs ) :?>
+          <li class="list-group-item ">
+            <?= $mhs['nama']; ?>
+            <a href=" <?= BASEURL; ?>/mahasiswa/hapus/ <?= $mhs['id']; ?>" class="badge badge-danger float-right ml-2 " onclick="return confirm('yakin?')">Hapus</a>
+            <a href=" <?= BASEURL; ?>/mahasiswa/ubah/ <?= $mhs['id']; ?>" class="badge badge-success float-right ml-2 tampilModalUbah" data-toggle="modal" data-target="#forModal" data-id="<?= $mhs['id']?>">Ubah</a>
+            <a href=" <?= BASEURL; ?>/mahasiswa/detail/ <?= $mhs['id']; ?>" class="badge badge-secondary float-right ml-2 ">Detail</a>
+          </li>
+      <?php endforeach; ?>
+        </ul>
+      </div>
+    </div>
+  </div>
 
-	<div class="row">
-		<div class="col-lg-6">
-			<button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
-				Tambah Data Mahasiswa
-			</button>
-			<br><br>
-			<h3>Daftar Mahasiswa</h3>
-			<ul class="list-group">
-				<?php foreach( $data['mhs'] as $mhs) :
-				?>
-			  <li class="list-group-item">
-			   <?= $mhs['nama']; ?>
-
-			   
-			   <a href="<?= BASEURL; ?>/mahasiswa/detail/ <?= $mhs['id']; ?>" class="badge badge-primary float-right ml-1">detail</a>
-
-			   <a href="<?= BASEURL; ?>/mahasiswa/ubah/ <?= $mhs['id']; ?>" class="badge badge-warning float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?= $mhs['id']; ?>">Ubah</a>
-
-
-			   <a href="<?= BASEURL; ?>/mahasiswa/hapus/ <?= $mhs['id']; ?>" class="badge badge-danger float-right ml-1" onclick="return confirm('yakin');">hapus</a>
-			  
-
-			  </li>
-			  <?php endforeach ?>
-			</ul>
-		
-		
-	</div>
-	
-</div>
 
 <!-- Modal -->
-<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+<div class="modal fade" id="forModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
+        
         <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
-		<div class="form-group">
-			<label for="nama">Nama </label>
-			<input type="text" class="form-control" id="nama" name="nama" >
- 	   </div>
 
-		<div class="form-group">
-			<label for="nrp">NRP</label>
-			<input type="number" class="form-control" id="nrp" name="nrp" >
- 	   </div>
+            <input type="hidden" name="id" id="id">
+            <div class="form-group">
+              <label for="nama">Nama</label>
+              <input type="text" class="form-control" id="nama" name="nama">
+            </div>
 
-		<div class="form-group">
-			<label for="emai">Email </label>
-			<input type="email" class="form-control" id="emai" name="emai" >
- 	   </div>
+            <div class="form-group">
+              <label for="nrp">Nrp</label>
+              <input type="number" class="form-control" id="nrp" name="nrp">
+            </div>
 
-		<div class="form-group">
-    		<label for="jurusan">Jurusan</label>
-    		<select class="form-control" id="jurusan" name="jurusan">
-				<option value="Teknik Informatika">Teknik Informatika</option>
-				<option value="Teknik Industri">Teknik Industri</option>
-				<option value="Kedokteran">Kedokteran</option>
-				</select>
-  		</div>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" class="form-control" id="email" name="email">
+            </div>
+
+            <div class="form-group">
+              <label for="jurusan">Jurusan</label>
+              <select class="form-control" id="jurusan" name="jurusan">
+                <option value="Teknik Informatika">Teknik Informatika</option>
+                <option value="Teknik Pangan">Teknik Pangan</option>
+                <option value="Kedokteran">Kedokteran</option>
+                <option value="Teknik Industri">Teknik Industri</option>
+                <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+              </select>
+            </div>
 
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Kembali</button>
         <button type="submit" class="btn btn-primary">Tambah Data</button>
-		</form>
       </div>
+      </form>
     </div>
   </div>
 </div>
-
